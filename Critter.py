3f76aa15fe6dbdc65ec6fe746c37b53b4449c78b
tribottriadE <tribotman@gmail.com>
2012-12-18 23:25:11 -0700

    Fixed errors

--- Critter.py
+++ Critter.py
@@ -1,7 +1,7 @@
 __author__ = "Jason and Evan"
 __version__ = 6.9
 
-'''Class representing the single type of critter in our life simular.  '''
+'''Class representing the single type of critter in our life simular. '''
 
 import random
 from Organism import Organism
@@ -21,7 +21,7 @@ class Critter(Organism):
         #Set sense of smell radius, hunger and mating thresholds, and energy-to-smell ratio
         self.smellRatio = smellRatio
         self.sense = senseOfSmell
-        self.hungerThreshold = HUNGER_THRESHOLD 
+        self.hungerThreshold = HUNGER_THRESHOLD
         self.matingThreshold = HUNGER_THRESHOLD + endurance
 
         #Sets self.hungry based on energy
@@ -35,7 +35,7 @@ class Critter(Organism):
 
         
     def move(self, critterTuple, fruitTuple):
-        '''Takes two tuples from the World, containing the total smell of critters and fruit (respectively) in each of the four cardinal directions.  The critter then moves toward direction with the strongest smell: if it is hungry, it seeks out fruit.  Otherwise, it seeks out other critters to reproduce.'''
+        '''Takes two tuples from the World, containing the total smell of critters and fruit (respectively) in each of the four cardinal directions. The critter then moves toward direction with the strongest smell: if it is hungry, it seeks out fruit. Otherwise, it seeks out other critters to reproduce.'''
         if self.hungry:
             #If the critter is hungry, it checks fruit-smells in each direction.
             self.pickHighest(fruitTuple)
@@ -46,7 +46,7 @@ class Critter(Organism):
 
         
     def processCollision(self, passed):
-        '''Takes any objects the critter has colided with from the World.  If it has collided with another critter, it returns the traits of any offspring they would have.  If it has collided with a fruit, it adds the fruit's energy to its own.  In both cases the World is responsible for creating/destroying critters and fruits.'''
+        '''Takes any objects the critter has colided with from the World. If it has collided with another critter, it returns the traits of any offspring they would have. If it has collided with a fruit, it adds the fruit's energy to its own. In both cases the World is responsible for creating/destroying critters and fruits.'''
         #If we were passed a critter, mate with it.
         if passed.__type__ == "Critter":
             self.mate(passed)
@@ -86,7 +86,7 @@ class Critter(Organism):
 
 
     def mate(self, otherCritter):
-        '''Given another critter, returns a tuple of randomly-chosen inherited traits for their child... if the critter is ready to mate.  Otherwise, it returns None.'''
+        '''Given another critter, returns a tuple of randomly-chosen inherited traits for their child... if the critter is ready to mate. Otherwise, it returns None.'''
         #If it ain't hungry, it's ready to mate!
         if not self.hungry:
             #The other two traits will be chosen randomly, with the values of its two parents used as the range of possible values.
@@ -107,7 +107,7 @@ class Critter(Organism):
 
     def randomBetween(self, value1, value2):
         '''Returns a random integer between the two specified values.'''
-        #random.triangular() requires that the range to choose from be in the form (smallest value, largest value).  This a quick and easy way to sort the two values so this is always true.
+        #random.triangular() requires that the range to choose from be in the form (smallest value, largest value). This a quick and easy way to sort the two values so this is always true.
         sortingList = [value1, value2]
         sortingList.sort()
 
@@ -120,14 +120,14 @@ class Critter(Organism):
 
 
     def eat(self, fruit):
-        '''Takes the energy of a given fruit and adds it to the critter's own energy.  Assumes that the World will destroy the fruit afterward.'''
+        '''Takes the energy of a given fruit and adds it to the critter's own energy. Assumes that the World will destroy the fruit afterward.'''
         self.energy += fruit.getEnergy()
         self.checkHunger()
         self.updateSmell()
 
 
     def pickHighest(self, choices):
-        '''Takes a list of smells for each direction, returns a value correpsonding to the index of the direction with the highest smell.  If multiple directions have the same smell value, the critter will choose randomly between them.'''
+        '''Takes a list of smells for each direction, returns a value correpsonding to the index of the direction with the highest smell. If multiple directions have the same smell value, the critter will choose randomly between them.'''
         directionList = []
 
         #Create a list of tuples, each containing a (smell value, direction it corresponds to) pair...
@@ -136,15 +136,15 @@ class Critter(Organism):
 
         #...Which makes it easy to choose the value with the strongest smell while remembering which direction it corresponds to.
         #sort() sorts by the first item in a tuple by default
-        choiceList.sort()
+        directionList.sort()
 
         #Figure out if several values in the list are equal - if so, we need to choose randomly between them.
         #Our choices can easily be represented as a range between 0 and [number of equal largest values] to choose from:
         choiceRange = 0
         
-        for i in range(1, len(choiceList)):
+        for i in range(1, len(directionList)):
             #If we've reached the point where an item is not equal to the first - and therefore highest - value, we don't make our range any bigger.
-            if choiceList[i][0] != choiceList[0][0]:
+            if directionList[i][0] != directionList[0][0]:
                 break
 
             else:
@@ -152,4 +152,4 @@ class Critter(Organism):
                 choiceRange += 1
 
         #Now, choose a random direction with the "best smell" and return its original index.
-        return choiceList[ int(random.triangular(0, choiceRange)) ][1]
+        return directionList[ int(random.triangular(0, choiceRange)) ][1]
