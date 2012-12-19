647981a3fed6cd3773de21fdadfe14b44eb840a5
tribottriadE <tribotman@gmail.com>
2012-12-18 20:25:56 -0700

    Update Critter.py
    Changed a couple function names, started commenting.

--- Critter.py
+++ Critter.py
@@ -8,22 +8,29 @@ MATING_THRESHOLD = 40
 HUNGER_THRESHOLD = 30
 METABOLISM = 1
 
-class critter(Organism):
+class Critter(Organism):
 
     def __init__(self, startingEnergy, senseOfSmell, startingSmell):
+        '''Creates a new critter with the specified energy level, sense of smell (as a radius), and smell strength.'''
+        #Initialize all the parameters contained in the Organism superclass
         Organism.__init__(self, startingEnergy, startingSmell, METABOLISM)
 
+        #Set sense of smell radius, hunger, and mating thresholds
         self.sense = senseOfSmell
         self.hungerThreshold = HUNGER_THRESHOLD 
         self.matingThreshold = MATING_THRESHOLD
+
+        #Sets self.hungry based on energy
         self.checkHunger()
 
 
     def getSmellDistance(self):
+        '''Returns the distance around the critter that it can smell to sense fruit and other critters.'''
         return self.sense
 
         
-    def decide(self, critterTuple, fruitTuple):
+    def move(self, critterTuple, fruitTuple):
+        '''Takes two tuples from the World, containing the total smell of critters and fruit (respectively) in each of the four cardinal directions.  The critter then moves toward direction with the strongest smell: if it is hungry, it seeks out fruit.  Otherwise, it seeks out other critters to reproduce.'''
 
         if self.hungry:
             self.pickHighest(fruitTuple)
@@ -31,23 +38,30 @@ class critter(Organism):
             self.pickHighest(critterTuple)
 
         
-    def iterate(self, passed):
+    def processCollision(self, passed):
+        '''Takes any objects the critter has colided with from the World.  If it has collided with another critter, it returns the traits of any offspring they would have.  If it has collided with a fruit, it adds the fruit's energy to its own.  In both cases the World is responsible for creating/destroying critters and fruits.'''
+        #If we were passed a critter, mate with it.
         if passed.__type__ == "Critter":
             self.mate(passed)
+            
+        #If we were passed a fruit, eat it.
         elif passed.__type__ == "Fruit":
             self.eat(passed)
+            
+        #Otherwise, do nothing.
         else:
             pass
 
-        self.loseEnergy()
-
 
     def loseEnergy(self):
+        '''Decreases the critter's energy based on its metabolic rate, returns the amount of energy lost, and updates whether or not it is hungry.'''
+        #Superclass method!
         Organism.loseEnergy(self)
         self.checkHunger()
 
         
     def checkHunger(self):
+        '''Updates whether or not the critter is hungry (if it isn't, it's ready to mate) based on its energy level.'''
         if not self.hungry:
             if self.energy < self.hungerThreshold:
                 self.hungry = True
@@ -89,5 +103,10 @@ class critter(Organism):
             
         choiceList.sort()
 
-        if choiceList[0][0] != -1:
+        #Negative values 
+        if choiceList[0][0] >= 0:
             return choiceList[0][1]
+
+
+    def isReadyToMate(self):
+        return not self.hungry
